e56898b107523645128304e45dd8a6a0e578e2ec
Jiayang Xu <jiayang.xu@marquette.edu>
2023-12-06 04:08:07 +0800

    project structure

--- FinalProject/Program.cs
+++ FinalProject/Program.cs
@@ -1,10 +1,9 @@
-﻿namespace FinalProject
+﻿namespace FinalProject;
+
+public class Program
 {
-    internal class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
+      
     }
 }
