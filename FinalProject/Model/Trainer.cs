8397770ca60d68cd7d06918f86237936b30357fd
Jiayang Xu <jiayang.xu@marquette.edu>
2023-12-06 05:05:18 +0800

    Add Trainer class

---
+++ FinalProject/Model/Trainer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinalProject.Model;
+
+public class Trainer
+{
+    public int TrainId { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public int Age { get; set; }
+    public char Gender { get; set; }
+    public string Type { get; set; }
+}
