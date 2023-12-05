5c83848010128a0a9844729b4a2f9d2af5f9eaff
wangx <xiayi.wang@marquette.edu>
2023-12-05 15:08:48 -0600

    member

--- FinalProject/Model/Member.cs
+++ FinalProject/Model/Member.cs
@@ -4,14 +4,15 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace FinalProject
+namespace FinalProject.Model
 {
     public class Member
     {
         public string FName { get; set; }
         public string LName { get; set; }
         public int Age { get; set; }
-        public int MemberID { get; set;}
+        public int MemberID { get; set; }
+
 
 
     }
