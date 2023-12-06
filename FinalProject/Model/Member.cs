9f33e9bd325be302f375656b59dd267ebae9c761
wangx <xiayi.wang@marquette.edu>
2023-12-06 14:50:19 -0600

    Member

--- FinalProject/Model/Member.cs
+++ FinalProject/Model/Member.cs
@@ -8,12 +8,24 @@ namespace FinalProject.Model
 {
     public class Member
     {
+        private static int acount;
         public string FName { get; set; }
         public string LName { get; set; }
         public int Age { get; set; }
         public int MemberID { get; set; }
+        public int password { get; set; }
+        public int Pnumber { get; set; }
+        public string address { get; set; }
+        public string Gender { get; set; }
+        public int EmergcyC {  get; set; }
 
 
+
+        public Member()
+        {
+            MemberID++;
+            acount = MemberID;
+        }
 
     }
 }
