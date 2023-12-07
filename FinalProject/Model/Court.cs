d61c3b5cbe253afdad13f53dbbedeae5aac92a40
Amory Tong <qian.tong@marquette.edu>
2023-12-06 20:07:17 -0600

    Court

--- FinalProject/Model/Court.cs
+++ FinalProject/Model/Court.cs
@@ -6,7 +6,17 @@ using System.Threading.Tasks;
 
 namespace FinalProject.Model
 {
-    public class Court
+    public abstract class Court // Common properties and methods for all courts
+
+    public TimeSpan OpeningTime { get; set; }
+    public TimeSpan ClosingTime { get; set; }
+
+    protected Court()
     {
+        OpeningTime = new TimeSpan(6, 0, 0);//6a.m.
+        ClosingTime = new TimeSpan(22, 0, 0);//10p.m.
+        NumberOfCourts = numberOfCourts;
     }
+
+    
 }
---
+++ FinalProject/Model/TennisCourt.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+public class TennisCourt：Court
+{
+
+    public TennisCourt() : base(6) // Tennis has 6 courts
+    {
+    }
+
+}
