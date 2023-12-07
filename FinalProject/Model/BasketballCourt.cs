c7238f03b5215e0c3965bd57ff0ccfea7af1e0ea
Amory Tong <qian.tong@marquette.edu>
2023-12-06 23:52:20 -0600

    list created

--- FinalProject/Model/BasketballCourt.cs
+++ FinalProject/Model/BasketballCourt.cs
@@ -1,9 +1,17 @@
-﻿using System;
+﻿using FinalProject.Model;
+using System;
+using System.Globalization;
 
-public class BasketballCourt：Court
+public class BasketballCourt:Court
 {
+    public string BasketballCourtName { get; set; }
+    public int BasketballCourtId { get; set; }
+    public bool IsAvailable { get; set; }
 
-    public BasketballCourt() : base(4) // Basketball has 4 courts
+    public BasketballCourt(int basketballcourtId, String basketballcourtName): base(basketballcourtId, basketballcourtName)
     {
+        BasketballCourtId = basketballcourtId;
+        BasketballCourtName = basketballcourtName;
+        IsAvailable = true;
     }
 }
