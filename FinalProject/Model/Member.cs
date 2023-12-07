917e9526bae06d16afe19643d9068846895bcfbc
Jiayang Xu <jiayang.xu@marquette.edu>
2023-12-07 14:15:48 +0800

    add members

--- FinalProject/Model/Member.cs
+++ FinalProject/Model/Member.cs
@@ -13,7 +13,7 @@ public class Member
     public int MemberId { get; set; }
     public string Name { get; set; }
     public int Age { get; set; }
-    public string Email { get; set; }
+    public string Password { get; set; }
     private string EmergencyContact { get; set; } 
     private static int account = 1000;
 
