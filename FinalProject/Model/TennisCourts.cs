54b5d033b274c5445cc0efbd9b75be5a33de336c
Amory Tong <qian.tong@marquette.edu>
2023-12-07 00:04:30 -0600

    List created

---
+++ FinalProject/Model/TennisCourts.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinalProject.Model
+{
+    public class TennisCourts
+    {
+        public List<TennisCourt> tenniscourts { get; set; }
+
+        public TennisCourts()
+        {
+            tenniscourts = new List<TennisCourt>
+            {
+                new TennisCourt(1, "TennisCourt1"),
+                new TennisCourt(2, "TennislCourt2"),
+                new TennisCourt(3, "TennisCourt3"),
+                new TennisCourt(4, "TennisCourt4"),
+                new TennisCourt(5, "TennisCourt5"),
+                new TennisCourt(6, "TennisCourt6")
+            };
+        }
+    }
+}
