dc045410a55d0c6688adc0147c4110950a8c315c
Xiayi Wang <xiayi.wang@marquette.edu>
2023-12-07 21:36:03 -0600

    basic session reservation

--- FinalProject/Model/Sessions.cs
+++ FinalProject/Model/Sessions.cs
@@ -24,4 +24,24 @@ public class Sessions
     {
         return sessions.Where(s => s.MemberId == memberId).ToList();
     }
+
+    public bool AddSessionReservation(int memberId, int trainerId, DateTime sessionDate)
+    {
+        // 检查是否存在时间冲突
+        bool hasConflict = sessions.Any(s => s.TrainerId == trainerId && s.SessionDate == sessionDate);
+        if (hasConflict)
+        {
+            return false; // 如果有冲突，则返回 false
+        }
+
+        var newSession = new Session (sessionDate,memberId, trainerId)
+        {
+            MemberId = memberId,
+            TrainerId = trainerId,
+            SessionDate = sessionDate
+        };
+
+        sessions.Add(newSession);
+        return true; // 如果没有冲突，则添加会话并返回 true
+    }
 }
