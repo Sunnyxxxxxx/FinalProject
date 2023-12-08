ecd72389de8910df7be0b9feff8cb4a5c5b38593
Amory Tong <qian.tong@marquette.edu>
2023-12-07 22:06:57 -0600

    comment

--- FinalProject/Model/Member.cs
+++ FinalProject/Model/Member.cs
@@ -9,7 +9,7 @@ using System.Threading.Tasks;
 namespace FinalProject.Model;
 
 public class Member
-{
+{//public member class and holds personal details for members inn system, including an identifier name ,age , and password
     public int MemberId { get; set; }
     public string FirstName { get; set; }
     public string LastName { get; set; }
@@ -19,11 +19,11 @@ public class Member
     private static int account = 1000;
 
     private Member(int age)
-    {
+    {// connstructor logic
 
     }
     public static Member CreateMember(string firstName, string lastName, string password, int age)
-    {
+    {// static method to create a new member instance, enforcing a minimum age requirement
         if (age < 6)
         {
             Console.WriteLine("Age must be 6 or above to be a member.");
@@ -31,7 +31,7 @@ public class Member
         }
 
         var newMember = new Member (age)
-        {
+        {// create a new member with the procided details and an auto account memberid
             FirstName = firstName,
             LastName = lastName,
             Password = password,
--- FinalProject/Model/MemberReservation.cs
+++ FinalProject/Model/MemberReservation.cs
@@ -7,12 +7,12 @@ using System.Threading.Tasks;
 namespace FinalProject.Model;
 
 public class MemberReservation
-{
+{// links a member with a reservation, representing a booking bby a member
         public Member member { get; set; }
         public Reservation reservation { get; set; }
 
         public MemberReservation (Member m, Reservation r)
-        {
+        {//constructor that takes a member object and the reservation object to create an association betwween them
             member = m;
             reservation = r;
         }
